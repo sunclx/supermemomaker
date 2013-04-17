a0ee84edc7ff1cbd5158217748acf738652a3331
sunclx <sunclx@gmail.com>
2013-04-17 21:20:24 +0800

    完善Course

--- supermemomaker.rb
+++ supermemomaker.rb
@@ -12,7 +12,8 @@ class Course
   <?xml version="1.0" encoding="utf-8"?><course xmlns="http://www.supermemo.net/2006/smux"></course>
 EOF
     document ||= string
-    @course = Document.new(document).root
+    @doc = Document.new(document)
+    @course = @doc.root
     @course << @guid = Element.new('guid')
     @guid.text= SecureRandom.uuid
     @course << @created = Element.new('created')
@@ -41,7 +42,7 @@ EOF
     @box_link.text= nil
     @course << @version = Element.new('version')
     @version.text = '1.0.3531'
-    @items = Items.new(self)
+    @items = Items.new(@course)
   end
 
   def to_s
@@ -61,52 +62,50 @@ EOF
     @items << item
   end
 
-  def pres
-    @course.get_elements('//element[@type="pres"]')
-  end
-
-  def add_exercise(id='1', name='blank', subtype=nil)
-    tmp = Element.new('element')
-    tmp.add_attributes(id: id.to_s, name: name, type: 'exercise', subtype: subtype)
-    @course<<tmp
-  end
-
-  def add_pres(element, name='blank', disabled='true', subtype=nil)
-    pa = get_elements("//element[@type='pres']")[0]
-    if pa
-      pa.add_element(element)
-    else
-      tmp = element.clone
-      element.add_attributes(id: '99999', name: name, type: 'pres', disabled: disabled, subtype: subtype)
-      element<<tmp
-    end
+  def makefile
+    File.open('course.xml', 'w') { |f| f.write(@doc) }
+    (1..10).each { |item|
+      File.open('item'+('%05d'%item)+'.xml', 'w') { |f|
+        f.write(item.to_s)
+      } }
   end
 end
 
 class Items
   def initialize(course)
-    @items ||= []
     @course = course
+    @items = @course.get_elements('//element[@type="exercise"]')
   end
 
-  def [](int)
-    @items[int]
+  def [](index=nil, id=nil)
+    @items.each { |e| return e if e.attributes['id']=id.to_s } if id.kind_of? Numeric
+    case index.class
+      when Numeric;
+        return @items[index]
+      when String, Symbol;
+        @items.each { |e| return e if e.attributes['name']=index.to_s }
+    end
   end
 
   def <<(item)
-    @course.course << item.item
+    @course<< item.item
+  end
+
+  def each
+    @items.each { |items| yield items }
   end
 end
 
-class Item < Document
-  def initialize(qa =nil, parent=nil, document=nil)
+class Item
+  def initialize(qa =nil, course=nil, document=nil)
     string = <<EOF
    <?xml version="1.0" encoding="utf-8"?><item xmlns="http://www.supermemo.net/2006/smux"></item>
 EOF
     document ||= string
-    @item = Document.new(document).root
-    @parent = parent || Course.new
-    @id = @parent.id_gen
+    @doc = Document.new(document)
+    @item=@doc.root
+    @course = course || Course.new
+    @id = @course.id_gen
     @item<<@lesson_title = Element.new('lesson-title')
     @lesson_title.text = lesson_title
     @item<<@chapter_title = Element.new('chapter-title')
@@ -119,17 +118,21 @@ EOF
     @modified.text = Time.now.to_s[0, 10]
     @item<<@template_id = Element.new('template-id')
     @template_id.text = '1'
-    @parent << self
+    @course << self
+    @pres = nil
   end
 
   def to_s
-    @item.to_s
+    @doc.to_s
   end
 
-  attr_reader :item, :lesson_title, :chapter_title, :question_title, :modified, :template_id, :parent, :id
+  attr_reader :item, :lesson_title, :chapter_title, :question_title, :modified, :template_id, :course, :id
   attr_accessor :question, :answer
-
-
+end
+class Pres < Item
+  "[id:id.to_s,name:name,type:'exercise',subtype:subtype]
+    [element, name='blank', disabled='true',subtype=nil]
+    [id:'99999',name:name,type:'pres',disabled:disabled,subtype:subtype]"
 end
 class QA
 
@@ -153,10 +156,13 @@ class Drap_drop < Block
   end
 end
 
-item1 = Item.new
-item2 = Item.new(nil, item1.parent)
-p item1.id
-p item2.id
-print item1.parent
+def makeQA(lines)
+  nil
+end
+
+course = Course.new
+File.foreach('dates.txt') { |line| Item.new(makeQA(line), course) }
+course.makefile
+
 p '%05d'% 123
 
