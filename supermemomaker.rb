bd88888885c69a1e026d1ba820495cfc4699c02e
sunclx <sunclx@gmail.com>
2013-04-19 10:30:02 +0800

    初步完成Course,Item，      设计了Block,SMText

--- supermemomaker.rb
+++ supermemomaker.rb
@@ -3,7 +3,7 @@ require 'securerandom'
 include REXML
 
 class Course
-  attr_accessor :course
+  attr_reader :course
 
   def initialize(file=nil)
     string = <<EOF
@@ -34,6 +34,7 @@ EOF
       e = @course.elements[k.to_s]
       e.text= v.to_s if e.text == nil or e.text == ''
     }
+    self.write_to_file
   end
 
   def to_s
@@ -44,8 +45,11 @@ EOF
     id = ((1..99999).to_a - self.id.sort!)[0]
     exercise = @course.add_element('element')
     exercise.add_attributes(id: id, type: 'exercise', name: name)
-    File.open(@file, 'w') { |f| f.write(self.to_s) }
     exercise
+  end
+
+  def write_to_file
+    File.open(@file, 'w') { |f| f.write(self.to_s) }
   end
 
   def id
@@ -54,29 +58,28 @@ EOF
   end
 
   def [](index)
-    @course.get_elements('//element[@type="exercise"]')[index]
+    exercises=@course.get_elements('//element[@type="exercise"]')
+    Item.new(self, exercises[index])
   end
 
   def <<(item)
-    @course<< item
-  end
-
-  def makefile
-    File.open('course.xml', 'w') { |f| f.write(@doc) }
-    (1..10).each { |item|
-      File.open('item'+('%05d'%item)+'.xml', 'w') { |f|
-        f.write(item.to_s)
-      } }
+    @course<< item.item
   end
 end
 
 class Item
-  def initialize(course, file=nil)
+  attr_reader :item
+
+  def initialize(course, exercise =nil, file=nil)
     string = <<EOF
 <?xml version="1.0" encoding="utf-8"?><item xmlns="http://www.supermemo.net/2006/smux"></item>
 EOF
     @name='blank0'
-    @exercise=course.exercise_gen(@name)
+    if exercise ==nil
+      @exercise=course.exercise_gen(@name)
+    else
+      @exercise = exercise
+    end
     @file = 'item'+'%05d'%self.id+'.xml'
     File.open(@file, 'w') { |f| f.write(string) } unless File.exist? @file
     @doc = Document.new(File.new(@file))
@@ -88,8 +91,21 @@ EOF
         'modified' => Time.now.to_s[0, 10],
         'template-id' => '1'
     }
-
+    @attributes.each_pair { |k, v|
+      @item.add_element(k.to_s) unless @item.elements[k.to_s]
+      e = @item.elements[k.to_s]
+      e.text= v.to_s if e.text == nil or e.text == ''
+    }
+    @item.add_element('question')
+    @item.add_element('answer')
     @pres = nil
+    self.write_to_file
+  end
+
+  def self.build(course, exercise=nil, file=nil)
+    item = self.new(course, exercise, file)
+    yield item if block_given?
+    item
   end
 
   def id
@@ -99,19 +115,79 @@ EOF
   def to_s
     @doc.to_s
   end
+
+  def qa
+    QA.new(@item.elements['question'], @item.elements['answer'])
+  end
+
+  def qa=(qa)
+    self << qa
+  end
+
+  def <<(qa)
+    @item.delete_element(@item.elements['//question'])
+    @item.delete_element(@item.elements['//answer'])
+    @item<<qa.element[0]
+    @item<<qa.element[1]
+  end
+
+  def write_to_file
+    File.open(@file, 'w') { |f| f.write(self.to_s) }
+  end
 end
-class Pres < Item
-  "[id:id.to_s,name:name,type:'exercise',subtype:subtype]
-    [element, name='blank', disabled='true',subtype=nil]
-    [id:'99999',name:name,type:'pres',disabled:disabled,subtype:subtype]"
-end
+
 class QA
+  attr_reader :element
 
+  def initialize(question=nil, answer=nil)
+    @element =[Element.new('question'), Element.new('answer')]
+    @q=@element[0].text = question.to_s
+    @a=@element[1].text = answer.to_s
+
+    def @q.<<(string)
+      @q.text += string
+    end
+
+    def @a.<<(string)
+      @a.text+=string
+    end
+  end
+
+  def q
+    @q.text
+  end
+
+  def q=(string)
+    @q.text = string
+  end
+
+  def a
+    @a.text
+  end
+
+  def a=(string)
+    @a.text = string
+  end
 end
+
 class Block
+  def initialize(text=nil)
+    @string = text
+  end
 
+  def to_s
+    @string.gsub!('<', '&lt;')
+    @string.gsub!('>', '&gt;')
+    @string.to_s
+  end
 end
-class Drap_drop < Block
+
+class SMtext < Block
+  def initialize(text)
+    @string = text
+  end
+end
+=begin class Drap_drop < Block
   def drap_drop(sentence, n, orientation='horizontal', dropsign='10')
     dd = Element.new('drap-drop')
     dd.add_attributes(orentation: orientation, dropsign: dropsign)
@@ -126,16 +202,18 @@ class Drap_drop < Block
     dd
   end
 end
+=end
 
-def makeQA(lines)
-  nil
-end
 
 course = Course.new
-Item.new(course)
-Item.new(course)
-print course.id
+File.foreach('dates.txt') { |line|
+  string = SMtext.new(line.chomp)
+  Item.build(course) { |item|
+    item.qa = QA.new(string, string)
+    item.write_to_file
+  } }
+
+course.write_to_file
 
 
-p '%05d'% 123
 
