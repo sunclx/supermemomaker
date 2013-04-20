3092f721c72bdfc6b7f3f3dd75a56274f3ae8655
sunclx <sunclx@gmail.com>
2013-04-20 11:07:22 +0800

    删除qa，简化结构。

--- supermemomaker.rb
+++ supermemomaker.rb
@@ -1,8 +1,8 @@
 require './course.rb'
 course = Course.new
 File.foreach('dates.txt') { |line|
-  string = SDrapDrop.new.sentence_with_n(line.chomp)
-  Item.new(course).set_qa(string.to_s, nil).write_to_file }
+  block = SDrapDrop.new.sentence_with_n(line.chomp)
+  Item.new(course).add_to_question(block).write_to_file }
 
 course.write_to_file
 
